787f642fb90d6df7701cab67c772e34aa6940fb9
Russ <russellcaughey@gmail.com>
2015-06-08 09:18:59 -0700

    Renamed scene to node

--- src/Level.js
+++ src/Level.js
@@ -1,8 +1,6 @@
 var AstroidField = require('./AstroidField');
-var Node = require('famous/core/node');
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var Surface = require('famous/');
-var Node = require('famous/core/node');
 var Size = require('famous/components/size');
 var PointLight = require('famous/webgl-renderables/lights/PointLight');
 var Color = require('famous/utilities/Color');
@@ -13,24 +11,24 @@ var size;
 var windowSizeX = window.innerWidth;
 var windowSizeY = window.innerHeight;
 
-function Level(scene){
-    currentScene = scene.addChild();
+function Level(node){
+    this.node = node.addChild();
     this.init();
 }
 
 Level.prototype.init = function() {
     // Add size component to current scene
-    size = new Size(currentScene);
-   
-    // Add light to the level
-    var lightNode = currentScene.addChild()
+    size = new Size(this.node);
+       
+    // Add light node to the level
+    var lightNode = this.node.addChild()
         .setAlign(0.5, 0.5, 0.5)
         .setPosition(0, 0, 250);
     var light = new PointLight(lightNode)
         .setColor(new Color('white'));
     
     // Create an astroid field
-    var astroidField = new AstroidField(currentScene);
+    var astroidField = new AstroidField(this.node);
     
     this.isInit = true;
     
